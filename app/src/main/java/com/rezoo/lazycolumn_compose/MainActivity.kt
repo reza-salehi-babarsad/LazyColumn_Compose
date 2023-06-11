bca6b560347fdc59a8e5c80ea9ddee721919d2ba
reza <rezasalehibabarsad@hotmail.com>
2023-06-11 21:03:27 +0330

    final

--- app/src/main/java/com/rezoo/lazycolumn_compose/MainActivity.kt
+++ app/src/main/java/com/rezoo/lazycolumn_compose/MainActivity.kt
@@ -3,49 +3,59 @@ package com.rezoo.lazycolumn_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rezoo.lazycolumn_compose.ui.theme.LazyColumn_ComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumn_ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyList()
         }
     }
 }
 
 @Composable
-fun myList() {
-    Column {
-        for (i in 1..50){
+fun MyList() {
+    LazyColumn{
+        items(5000){
             Text(
-                text = "Item"
+                text = "Item $it",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
             )
         }
+
     }
-    
+
+}
+@Preview(showBackground =  true)
+@Composable
+fun MyListPrev() {
+   LazyColumn_ComposeTheme() {
+       MyList()
+   }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LazyColumn_ComposeTheme {
-        Greeting("Android")
-    }
-}
